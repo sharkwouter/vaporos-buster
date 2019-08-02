c42714f4e6d85a111a7a02f880354987ee1ebb3f
Wouter Wijsman <wwijsman@live.nl>
2019-08-03 01:35:16 +0200

    Big update to default.preseed and post_install.sh Removed some packages from the base as well which weren't working

--- additions/post_install.sh
+++ additions/post_install.sh
@@ -4,15 +4,14 @@
 chroot /target adduser --gecos "" --disabled-password steam
 chroot /target usermod -a -G desktop,audio,dip,video,plugdev,netdev,bluetooth,pulse-access steam
 chroot /target usermod -a -G pulse-access desktop
+chroot /target ln -sf /etc/systemd/system/lightdm.service /etc/systemd/system/display-manager.service
 cat - > /target/usr/share/lightdm/lightdm.conf.d/20_steamos.conf << 'EOF'
-[SeatDefaults]
+[Seat:*]
 pam-service=lightdm-autologin
 autologin-user=steam
 autologin-user-timeout=0
 EOF
 
-cp -r /cdrom/recovery /target/boot > /target/var/log/post_install.log
-mv /target/boot/recovery/live /target/boot/recovery/live-hd
 chroot /target date > /target/etc/skel/.imageversion
 cp /target/etc/skel/.imageversion /target/home/steam/.imageversion
 
@@ -53,11 +52,9 @@ dbus-send --system --type=method_call --print-reply --dest=org.freedesktop.Accou
 dbus-send --system --type=method_call --print-reply --dest=org.freedesktop.Accounts /org/freedesktop/Accounts/User1001 org.freedesktop.Accounts.User.SetXSession string:steamos
 systemctl enable build-dkms
 (for i in `dkms status | cut -d, -f1-2 | tr , / | tr -d ' '`; do sudo dkms remove $i --all; done) | zenity --progress --no-cancel --pulsate --auto-close --text="Configuring Kernel Modules" --title="SteamOS Installation"
-plymouth-set-default-theme -R steamos
+#plymouth-set-default-theme -R steamos
 update-grub
 grub-set-default 0
-# boot into recovery partition on the next boot
-grub-reboot "Capture System Partition"
 passwd --delete desktop
 rm /etc/sudoers.d/post_logon
 rm /usr/bin/post_logon.sh && reboot
@@ -99,7 +96,7 @@ GRUB_DEFAULT=saved
 GRUB_HIDDEN_TIMEOUT_QUIET=true
 GRUB_DISTRIBUTOR=\`lsb_release -i -s 2> /dev/null || echo Debian\`
 GRUB_CMDLINE_LINUX=""
-GRUB_BACKGROUND=/usr/share/plymouth/themes/steamos/steamos_branded.png
+#GRUB_BACKGROUND=/usr/share/plymouth/themes/steamos/steamos_branded.png
 GRUB_DISABLE_LINUX_RECOVERY="true"
 GRUB_GFXMODE=auto
 EOF
@@ -111,11 +108,6 @@ echo "GRUB_TIMEOUT=5" >> /target/etc/default/grub
 fi
 
 
-# Add system partition backup/restore to the boot menu
-RECOVERYPARTITION=`mount | grep "/target/boot/recovery " | cut -f1 -d' '`
-ROOTPARTITION=`mount | grep "/target " | cut -f1 -d' ' | cut -f3- -d'/'`
-SWAPPARTITION=`tail -1 /proc/swaps | cut -f1 -d' '`
-if test -n "${RECOVERYPARTITION}" && test -n "${ROOTPARTITION}" && test -n "${SWAPPARTITION}"; then
 if test -d /sys/firmware/efi/; then
 ISEFI=Y
 else
@@ -127,40 +119,4 @@ if test "${ISEFI}" = "Y"; then
 echo "GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash\"" >> /target/etc/default/grub
 else
 echo "GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash vga=0x0318\"" >> /target/etc/default/grub
-fi
-
-cat - >> /target/etc/grub.d/40_custom << EOF
-menuentry "Capture System Partition"{
-  search --set -f /live-hd/vmlinuz
-EOF
-if test "${ISEFI}" = "Y"; then
-echo "  fakebios" >> /target/etc/grub.d/40_custom
-fi
-cat - >> /target/etc/grub.d/40_custom << EOF
-  linux /live-hd/vmlinuz boot=live union=overlay username=user config components quiet noswap edd=on nomodeset nodmraid noeject noprompt locales="en_US.UTF-8" keyboard-layouts=NONE ocs_prerun="mount ${RECOVERYPARTITION} /home/partimag" ocs_live_run="ocs-sr -q2 -j2 -z1p -i 2000 -sc -p true saveparts steambox ${ROOTPARTITION}" ocs_live_extra_param="" ocs_live_batch=no vga=788 ip=frommedia   live-media-path=/live-hd bootfrom=${SWAPPARTITION} toram=filesystem.squashfs i915.blacklist=yes radeonhd.blacklist=yes nouveau.blacklist=yes vmwgfx.enable_fbdev=no
-  initrd /live-hd/initrd.img
-}
-menuentry "Restore System Partition"{
-  search --set -f /live-hd/vmlinuz
-EOF
-if test "${ISEFI}" = "Y"; then
-echo "  fakebios" >> /target/etc/grub.d/40_custom
-fi
-cat - >> /target/etc/grub.d/40_custom << EOF
-  linux /live-hd/vmlinuz boot=live union=overlay username=user config components quiet noswap edd=on nomodeset nodmraid noeject noprompt locales="en_US.UTF-8" keyboard-layouts=NONE ocs_prerun="mount ${RECOVERYPARTITION} /home/partimag" ocs_live_run="ocs-sr -e1 auto -e2 -r -j2 -k -p reboot restoreparts steambox ${ROOTPARTITION}" ocs_live_extra_param="" ocs_live_batch=no vga=788 ip=frommedia   live-media-path=/live-hd bootfrom=${SWAPPARTITION} toram=filesystem.squashfs i915.blacklist=yes radeonhd.blacklist=yes nouveau.blacklist=yes vmwgfx.enable_fbdev=no
-  initrd /live-hd/initrd.img
-}
-menuentry "Clonezilla live"{
-  search --set -f /live-hd/vmlinuz
-EOF
-if test "${ISEFI}" = "Y"; then
-echo "  fakebios" >> /target/etc/grub.d/40_custom
-fi
-cat - >> /target/etc/grub.d/40_custom << EOF
-  linux /live-hd/vmlinuz boot=live union=overlay username=user config components quiet noswap edd=on nomodeset nodmraid noeject noprompt locales="en_US.UTF-8" keyboard-layouts=NONE ocs_prerun="mount ${RECOVERYPARTITION} /home/partimag" ocs_live_run="ocs-live-general" ocs_live_extra_param="" ocs_live_batch=no vga=788 ip=frommedia  nosplash  live-media-path=/live-hd bootfrom=${SWAPPARTITION} toram=filesystem.squashfs i915.blacklist=yes radeonhd.blacklist=yes nouveau.blacklist=yes vmwgfx.enable_fbdev=no
-  initrd /live-hd/initrd.img
-}
-EOF
-else
-echo "Missing one of /, /boot/recovery, or swap. Disabling recovery partition support"
 fi
